dbfb3a2a3ec52349f3a32acb6b1f25add83c5a81
ImtiazAkash <imtiaz.atick07@gmail.com>
2021-07-02 22:35:52 +0600

    notun kisu add kori

--- CourierManagementSystem/src/main/java/servlets/AkashJohn.java
+++ CourierManagementSystem/src/main/java/servlets/AkashJohn.java
@@ -5,6 +5,9 @@ public class AkashJohn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// and his name is johncena. and this is a new line to test if it shares across our team
+		// hey do you read me nag
+		//bal chal
+		
 	}
 
 }
